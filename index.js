40d8959e208e3ac2f479315a5d2a65df5c7c0dbc
fedora <ishmaelmafole@gmail.com>
2023-10-21 22:41:14 +0200

    populated js for social buttons

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-//js Code comes here
+let twitter = document.querySelector('#twitter');
+let whatsapp = document.querySelector('#whatsapp');
+let facebook = document.querySelector('#fb');
+let phone = document.querySelector('#phone');
